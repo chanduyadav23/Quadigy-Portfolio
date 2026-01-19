446cb9a78245611c45930c62632e7678fef51110
chanduyadav23 <chanduy421@gmail.com>
2026-01-19 11:56:04 +0000

    changes

--- reactdemo/src/main.js
+++ reactdemo/src/main.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-<BrowserRouter>
-  <App />
-</BrowserRouter>
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
