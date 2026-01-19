2ed19916edb519cfad702dd5759a8b92d3f6b5a5
chanduyadav23 <chanduy421@gmail.com>
2026-01-19 12:06:25 +0000

    changes

--- reactdemo/src/App.js
+++ reactdemo/src/App.js
@@ -1,28 +1,28 @@
 // src/App.js
-import React, { Suspense } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
 
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import DetailPage from './pages/DetailPage';
-import RequireAuth from './components/RequireAuth';
-import { AuthProvider } from './context/AuthContext';
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import DetailPage from "./pages/DetailPage";
+import RequireAuth from "./components/RequireAuth";
+import { AuthProvider } from "./context/AuthContext";
 
-const Admin = React.lazy(() => import('./pages/Admin'));
-const AdminLogin = React.lazy(() => import('./pages/AdminLogin'));
+const Admin = React.lazy(() => import("./pages/Admin"));
+const AdminLogin = React.lazy(() => import("./pages/AdminLogin"));
 
 function App() {
   return (
     <AuthProvider>
+      {/* ✅ Navbar Router ke andar aayega */}
       <Navbar />
+
       <Suspense fallback={<div style={{ padding: 16 }}>Loading...</div>}>
         <Routes>
-          {/* Public */}
           <Route path="/" element={<Home />} />
           <Route path="/portfolio/:id" element={<DetailPage />} />
           <Route path="/login" element={<AdminLogin />} />
 
-          {/* Protected */}
           <Route
             path="/admin"
             element={
@@ -31,6 +31,7 @@ function App() {
               </RequireAuth>
             }
           />
+
           <Route
             path="/admin/:secret"
             element={
@@ -40,7 +41,6 @@ function App() {
             }
           />
 
-          {/* Fallback */}
           <Route path="*" element={<Home />} />
         </Routes>
       </Suspense>
